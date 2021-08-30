38216aa68e525ab03d5402c827ae4df61bb39580
SEIDN <xii0712@gmail.com>
2021-08-30 21:47:14 +0900

    makeing slider

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
-    $('.slider').bxSlider({
+    $('.mainSlider .slider').bxSlider({
         auto: true,
         autoHover: true,
         speed: 750,
         controls: false,
         mode: 'fade'
-    });   
+    });
+    $('.jobIntro.cra .slider').bxSlider({
+        mode: 'vertical',
+        maxSlides: 4,
+        minSlides: 4,
+        moveSlides: 2,
+        hideControlOnEnd: true,
+        infiniteLoop: false,
+        touchEnabled: false,
+        controls: true,
+        nextSelector: '.sliderNext',
+        prevSelector: '.sliderPrev',
+    });
 })
