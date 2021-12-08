810ccb4543fded565b9464bef724c6627ca4abc6
SEIDN <xii0712@gmail.com>
2021-12-08 23:35:20 +0900

    where to start?

---
+++ Renew/js/mainslider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".mainSlider .slider").bxSlider({
+        auto: true,
+        autoHover: true,
+        speed: 750,
+        controls: false,
+        mode: 'fade'
+    });
+});
