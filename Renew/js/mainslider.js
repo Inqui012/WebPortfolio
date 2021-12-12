77cb68301a9718b2b521bfbdc044d201926cc9b1
Euna Jung <xii0712@gmail.com>
2021-12-12 23:34:17 +0900

    Delete Renew directory

--- Renew/js/mainslider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".mainSlider .slider").bxSlider({
-        auto: true,
-        autoHover: true,
-        speed: 750,
-        controls: false,
-        mode: 'fade'
-    });
-});
